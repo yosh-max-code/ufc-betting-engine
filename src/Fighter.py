b274126935ec88f6c3ac1e66ea804047612ac6a7
YOSH <yash.sujit.singh@outlook.com>
2026-06-05 20:31:28 +0100

    Added -> type hints and getter(@property) and setter methods to see if valid data

--- src/Fighter.py
+++ src/Fighter.py
@@ -4,16 +4,30 @@
 class Fighter:
     #fighter class method 
 
-    def __init__(self, name, age, weight_class, record):
-        #these are attributes so no brackets needed when calling it
+    def __init__(self, name :str, age : int, weight_class: str, record:str) -> None: #adding type hints to show what ouput will show    
+
+                #these are attributes so no brackets needed when calling it
         self.name = name
-        self.age = age
+        self.age = age #underscore indication to use @property instead
         self.weight_class = weight_class
         self.record = record
 
+    #getter method for correct age
+    @property
+    def age(self) -> int:
+        return self._age
+        
+        #code setter block to check if age is in valid range
+    @age.setter
+    def age(self, value: int) -> None:
+        if value < 18 or value > 50:
+            raise ValueError('Not in valid range')
+        else:
+            self._age = value
+
     #Method , can only be called with brackets so .summary()
-    def summary(self):
-        return(f'{self.name} | Age: {self.age} | {self.weight_class} | W/L/D: {self.record}')
+    def summary(self)-> str: #type hint for method
+        return(f'{self.name} | Age: {self._age} | {self.weight_class} | W/L/D: {self.record}') 
 
 p1 = Fighter('Alex Perreira', 36, 'Heavyweight', '13-3-0')
 p2 = Fighter('Ilia Topuria', 29, 'Lightweight', '17-0-0' )
@@ -23,7 +37,7 @@ p2 = Fighter('Ilia Topuria', 29, 'Lightweight', '17-0-0' )
 #when inheriting always put in brackets where I am taking the attributes
 class Champion(Fighter):
     #champion which inherits att from fighter
-    def __init__(self, name, age, weight_class, record, title):
+    def __init__(self, name: str, age: int, weight_class: str, record: str, title: str) -> None: #adding type hints to show what ouput will show
         #super(). method used to call parent init and repeat functions 
         super().__init__(name, age, weight_class, record)
         self.title = title
@@ -31,3 +45,5 @@ class Champion(Fighter):
 c1 = Champion('Islam Makhachev', 34, 'Welterweight', '28-1-0', 'Champion')
 print(c1.summary())
 print(c1.title)
+
+
