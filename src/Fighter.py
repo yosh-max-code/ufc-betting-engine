975172dfa6f66c5024bdac38c2452e9a59daab5f
YOSH <yash.sujit.singh@outlook.com>
2026-06-07 18:08:41 +0100

    added @staticmethod which is pure caluclation in the class no need of self or cls

--- src/Fighter.py
+++ src/Fighter.py
@@ -29,8 +29,20 @@ class Fighter:
 
     #class method for whole class to parse
     @classmethod
-    def from_dict(cls, data:dict):
+    def from_dict(cls, data:dict): #cls instead of self
         return cls(data['name'], data['age'],data['weight_class'], data['record'])
+    
+    #static method doesnt need self or cls as its utility for pure calc
+    #if fighter has a record of 10 or more wins he is eligible for a title 
+    @staticmethod
+    def is_title_eligible(record: str): 
+        win_loss = record.split('-')
+        wins = int(win_loss[0])
+        if wins >= 10:
+            return True
+        else:
+            return False 
+
 
     #Method , can only be called with brackets so .summary()
     def summary(self)-> str: #type hint for method
@@ -53,7 +65,17 @@ c1 = Champion('Islam Makhachev', 34, 'Welterweight', '28-1-0', 'Champion')
 print(c1.summary())
 
 
+"""
 fighter_data = {"name": "Islam Makhachev", "age": 32, "weight_class": "Lightweight", "record": "28-1-0"}
 #centralised parsing to prep for data received in messy formats [ENCAPSULATION]
 D1 = Fighter.from_dict(fighter_data) 
 print(D1.summary())
+"""
+
+#checking if W/L record is title eligible need Wins > 9
+"""
+t1 = Fighter.is_title_eligible('10-1-0')
+t2 = Fighter.is_title_eligible('9-1-0')
+print(t1)
+print(t2)
+"""
