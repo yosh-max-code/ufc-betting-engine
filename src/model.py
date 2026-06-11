eb955ed7cf7523bb69e2912494450944f0b04859
YOSH <yash.sujit.singh@outlook.com>
2026-06-11 18:08:10 +0100

    added a new predictor XGboost as its productionm standard and can compare with LR as a basline to prove its more efficient with use of treeexpansion and error refinement

--- src/model.py
+++ src/model.py
@@ -5,19 +5,22 @@ from sklearn.linear_model import LogisticRegression
 from sklearn.compose import ColumnTransformer
 from sklearn.preprocessing import OneHotEncoder
 
+from xgboost import XGBClassifier
+
+
+
 
 class FighterPredictor:
-    def __init__(self) -> None:
+    def __init__(self, model=LogisticRegression()) -> None:
+        
+        
 
         self.preprocessor = ColumnTransformer(transformers=[("scaler", StandardScaler(), ["age", "strike_accuracy"]),
                                                             ("encoder", OneHotEncoder(), ["stance", "weight_class"])])
-        #ENCODE non numerical catergories into numbers
-
-
-
+        #ENCODE non numerical catergories into numbers i.e stance, weightclass
 
         self.pipeline = Pipeline(steps=[("preprocessor", self.preprocessor), 
-                                        ("classifier", LogisticRegression())])
+                                        ("classifier", model)])
     
     #StandardScaler rescales every feature so they're all on the same playing field. No stat bullies the others.
     #LogisitcRegression a classification model. You feed it fighter stats and it outputs a probability between 0 and 1
@@ -30,5 +33,9 @@ class FighterPredictor:
     def predict_proba(self, x):
         return self.pipeline.predict_proba(x)
         #returns a win probability for a new matchup
+
+lr_predictor = FighterPredictor(model=LogisticRegression())
+xgb_predictor = FighterPredictor(model=XGBClassifier())
+         
 
 
