37b9ab914395076c2107920cc69f8cba6cc0d9cf
YOSH <yash.sujit.singh@outlook.com>
2026-06-19 20:40:51 +0100

    added notebook of fake fighter data to test all methods in models such as LOG loss, AUC, calibration curve plotting, pipeline working with actual vs predicted probabilities

--- src/model.py
+++ src/model.py
@@ -14,7 +14,7 @@ import pandas as pd
 
 from sklearn.calibration import calibration_curve
 from typing import Tuple
-
+import matplotlib.pyplot as plt 
 class FighterPredictor:
     def __init__(self, model=LogisticRegression()) -> None:
         
@@ -27,7 +27,7 @@ class FighterPredictor:
         self.pipeline = Pipeline(steps=[("preprocessor", self.preprocessor), 
                                         ("classifier", model)])
         
-        self.calibrated_pipeline = CalibratedClassifierCV(estimator=self.pipeline, method="sigmoid")
+        self.calibrated_pipeline = CalibratedClassifierCV(estimator=self.pipeline, method="sigmoid", cv=2)
 
     
     #StandardScaler rescales every feature so they're all on the same playing field. No stat bullies the others.
@@ -51,17 +51,27 @@ class FighterPredictor:
     def evaluate_logloss(self, x: pd.DataFrame, y_true: pd.Series) -> float:
         y_pred_proba = self.predict_proba_calibrated(x)
         return log_loss(y_true, y_pred_proba)
-        #LOG LOSS CALCULATION
+        #LOG LOSS CALCULATION FOR CONFIDENCE
 
     def evaluate_auc(self, x: pd.DataFrame, y_true: pd.Series) -> float:
-        y_pred_proba = self.predict_proba_calibrated(x)
+        y_pred_proba = self.predict_proba_calibrated(x)[:, 1]
         return roc_auc_score(y_true, y_pred_proba)
+        #ROC AUC CALC FOR RANKINGS
 
 
     def get_calibration_curve(self, x:pd.DataFrame, y_true: pd.Series) -> Tuple[np.ndarray, np.ndarray]:
         y_pred_proba = self.predict_proba_calibrated(x)
         return calibration_curve(y_true, y_pred_proba)
+        #CALIBRATION CURVE USING TWO ARRAYS ONE ON REAL Y AND ONE ON PREDICTED Y    
 
+    def plot_calibration_curve(self, x:pd.DataFrame, y_true:pd.Series) -> None:
+        y_pred_proba = self.predict_proba_calibrated(x)[:, 1]
+        actual_probs, predicted_probs = calibration_curve(y_true, y_pred_proba)
+        plt.plot(predicted_probs, actual_probs, marker="o")
+        plt.plot([0, 1], [0, 1], linestyle="--") #REFERENCE LINE CALIBRATION
+        plt.xlabel("predicted probability")
+        plt.ylabel("actual probability")
+        plt.show()
 
 lr_predictor = FighterPredictor(model=LogisticRegression()) 
 xgb_predictor = FighterPredictor(model=XGBClassifier())
